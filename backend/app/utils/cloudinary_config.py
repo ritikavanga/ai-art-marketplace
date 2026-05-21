c8e1206d2d8a2e87921a2833afeac9a3e61a250b
ritikavanga <vangaritika14@gmail.com>
2026-05-21 20:22:36 +0530

    fix: resolved artwork upload errors

--- backend/app/utils/cloudinary_config.py
+++ backend/app/utils/cloudinary_config.py
@@ -1,7 +1,7 @@
 import cloudinary
 
 cloudinary.config(
-    cloud_name="Root",
+    cloud_name="do3qjkizj",
     api_key="866435645545138",
     api_secret="iJld1flUPSXj5SsUsN2i5rrGGls",
     secure=True
