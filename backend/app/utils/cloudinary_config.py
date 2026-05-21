e851189c6eb11459d79e8b03a12a86967dc9f72d
ritikavanga <vangaritika14@gmail.com>
2026-05-21 20:12:46 +0530

    feat: implemented artwork upload system

--- backend/app/utils/cloudinary_config.py
+++ backend/app/utils/cloudinary_config.py
@@ -2,6 +2,7 @@ import cloudinary
 
 cloudinary.config(
     cloud_name="Root",
-    api_key="iJld1flUPSXj5SsUsN2i5rrGGls",
-    api_secret="866435645545138"
+    api_key="866435645545138",
+    api_secret="iJld1flUPSXj5SsUsN2i5rrGGls",
+    secure=True
 )
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database.database import engine, Base
 
 from app.models.user import User
@@ -9,6 +11,17 @@ from app.routes import auth, artwork
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(artwork.router)
